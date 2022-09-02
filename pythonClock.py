36974a16d61c4094833be5e8f3410ff09dab1d88
veronicaglh <veronicagt.IT@gmail.com>
2022-09-02 19:51:44 +0300

    add hour,minute and second hand to clock

--- pythonClock.py
+++ pythonClock.py
@@ -48,3 +48,32 @@ def draw_clock(hour, minute, second):
         pennie.penup()
         pennie.goto(0, 0)
         pennie.right(30)
+
+    # Drawing the hands of the clock
+    pennie.penup()  # to draw the hour hand
+    pennie.goto(0, 0)
+    pennie.color('blue')
+    pennie.setheading(90)
+    angle = (hour / 12) * 360
+    pennie.right(angle)
+    pennie.pendown()
+    pennie.forward(38)
+
+    pennie.penup()  # to draw the minute hand
+    pennie.goto(0, 0)
+    pennie.color('purple')
+    pennie.setheading(90)
+    angle = (minute / 60) * 360
+    pennie.right(angle)
+    pennie.pendown()
+    pennie.forward(56)
+
+    pennie.penup()  # to draw the second hand
+    pennie.goto(0, 0)
+    pennie.pensize(1)
+    pennie.color('black')
+    pennie.setheading(90)
+    angle = (second / 60) * 360
+    pennie.right(angle)
+    pennie.pendown()
+    pennie.forward(30)
