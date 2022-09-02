26005316ea2e45e645a4d04843c529a135ed4834
veronicaglh <veronicagt.IT@gmail.com>
2022-09-02 19:41:55 +0300

    create the screen

--- pythonClock.py
+++ pythonClock.py
@@ -1,1 +1,13 @@
 # This program will build a working clock using python.
+
+# Import statements
+import turtle
+import time
+
+# Creating the screen
+window = turtle.Screen()
+window.bgcolor('violet')
+window.setup(width=500, height=500)
+window.tracer(0)
+
+pennie = turtle.Turtle()
