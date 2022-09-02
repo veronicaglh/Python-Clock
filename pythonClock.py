33f9879722c2ac87d9bec4def8e816af71be01fe
veronicaglh <veronicagt.IT@gmail.com>
2022-09-02 19:48:51 +0300

    place title on screen

--- pythonClock.py
+++ pythonClock.py
@@ -10,4 +10,17 @@ window.bgcolor('violet')
 window.setup(width=500, height=500)
 window.tracer(0)
 
+# Creating turtle pen named pennie to draw the clock
 pennie = turtle.Turtle()
+
+# Creating separate turtle pen named title to place the title on the screen
+title = turtle.Turtle()
+title.penup()
+title.goto(-75,160)
+title.pendown()
+title.write("Python Clock",font=("Calibri",22, "bold"))
+title.hideturtle()
+title.penup()
+title.goto(-30,150)
+title.pendown()
+title.write("A working clock",font=("Calibri",13, "italic"))
